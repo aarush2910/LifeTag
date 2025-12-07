565f888e543cfa1eb57bd61cd85d6d56cb6e3ea5
ayush2113 <agarwalayush8900@gmail.com>
2025-12-07 13:47:39 +0530

    changes some pydnatic v1 code to v2 in schemas

--- fastapi/app/schemas/vet_vactination.py
+++ fastapi/app/schemas/vet_vactination.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , ConfigDict
 from typing import Optional
 from datetime import date
 
@@ -16,5 +16,4 @@ class VetEventCreate(VetEventBase):
 class VetEventResponse(VetEventBase):
     id: str
 
-    class Config:
-        from_attributes = True  # updated from orm_mode
+    model_config = ConfigDict(from_attributes=True)
